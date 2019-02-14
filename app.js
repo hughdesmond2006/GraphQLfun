ee110ed2b6a38ae5f66305503eafcf5b5c67878d
hughdesmond2006 <hughdesmond2006@yahoo.ie>
2019-02-14 18:52:33 +0700

    graphQL basic query & mutation working

--- app.js
+++ app.js
@@ -1,4 +1,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphHttp = require('express-graphql');
+const {buildSchema} = require('graphql');    //es6 object destructuring: stores values of props to equally named vars
 
 const app = express();
+
+app.use(bodyParser.json());
+
+//----------- GraphQL -----------------
+//String = list of strings, ! = not null
+//Query named as 'objectname' EG. events
+//Mutation named as 'actionObjectname' EG. createEvent()
+//Resolver named same as above, they must match! EG. events
+//note: args can be passed to both queries and mutations
+app.use('/graphql', graphHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['mtbing', 'poker', 'gaming'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
+
+app.listen(3000);
